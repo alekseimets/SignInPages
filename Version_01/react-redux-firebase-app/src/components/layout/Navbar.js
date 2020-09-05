ccc3ad7b234f2be80f0681b91885cf003390e472
alekseimets <alekseimets@gmail.com>
2020-09-05 21:10:26 +0300

    Added Navbar.js imr, Navbar container Link

---
+++ Version_01/react-redux-firebase-app/src/components/layout/Navbar.js
@@ -0,0 +1,10 @@
+import {React} from "react";
+import {Link} from "react-router-dom"
+
+const Navbar = () => {
+    <nav className="nav-wrapper grey darken-3">
+        <div className="container">
+            <Link to="/" className="brand-logo">AlekseiPlan</Link>
+        </div>
+    </nav>
+}   
