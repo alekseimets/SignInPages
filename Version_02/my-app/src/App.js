34dd0d2f7bef4730ddee5af3796f4be5f1b64541
alekseimets <alekseimets@gmail.com>
2020-09-07 11:19:00 +0300

    Added Navbar and App inside

--- Version_02/my-app/src/App.js
+++ Version_02/my-app/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/layout/Navbar";
 
-class App extends Component() {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>AlekseiPlan</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
