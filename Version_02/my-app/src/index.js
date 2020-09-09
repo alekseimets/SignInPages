2324c3bc1b91c27738205508567ba076884fce8c
alekseimets <alekseimets@gmail.com>
2020-09-09 10:04:28 +0300

    Update index.js

--- Version_02/my-app/src/index.js
+++ Version_02/my-app/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createRedux} from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import rootReaducer from "./store/reducers/rootReducer";
+import {Provider} from "react-redux";
 
-const store =  createStore();
+const store = createStore(rootReaducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
