4a20e226877237e5b5a2d88d8e4cc995f8970d23
alekseimets <alekseimets@gmail.com>
2020-09-09 21:59:18 +0300

    Added In fbConfig content

---
+++ Version_02/my-app/src/config/fbConfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAGVEX52bWHKWbRx5ZZrZ87CU5cjSCvx3s",
+    authDomain: "signinpages-37998.firebaseapp.com",
+    databaseURL: "https://signinpages-37998.firebaseio.com",
+    projectId: "signinpages-37998",
+    storageBucket: "signinpages-37998.appspot.com",
+    messagingSenderId: "52874718518",
+    appId: "1:52874718518:web:7b19b16b7590e944aa5797",
+    measurementId: "G-YDY16GXYFH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  firebase.firestore().settings({timestampsInSnapshots: true})
+
+  export default firebase
