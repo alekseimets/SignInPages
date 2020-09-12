706b93e12e4b94c7aabe0948b6dbe0b1bea8e27c
alekseimets <alekseimets@gmail.com>
2020-09-13 00:34:54 +0300

    Update ProjectDetail

--- Version_02/my-app/src/Components/projects/ProjectSummary.js
+++ Version_02/my-app/src/Components/projects/ProjectSummary.js
@@ -3,7 +3,7 @@ import React from "react";
 const ProjectSummary = ({project}) => {
   return (
     <div className="card z-depth-0 project-summary">
-      <div className="card-container gray-text text-darken-3">
+      <div className="card-content grey-text text-darken-4">
         <span className="card-title">{project.title}</span>
         <p>Posted by the Net Ninja</p>
         <p className="grey-text">3rd September, 2am</p>
