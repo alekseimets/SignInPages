060f301c731150cf62b93cb0ebbd7d209802fd31
alekseimets <alekseimets@gmail.com>
2020-09-13 22:53:22 +0300

    Update SignedInLinks and authReducer also authActions

--- Version_02/my-app/src/Components/layout/SignedInLinks.js
+++ Version_02/my-app/src/Components/layout/SignedInLinks.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import {connect} from 'react-redux';
+import {signOut} from '../../store/action/authActions';
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
+  props.signOut
   return (
     <ul className="right">
       <li>
         <NavLink to="/create">New Project</NavLink>
       </li>
       <li>
-        <NavLink to="/">Log Out</NavLink>
+        <a onClick={props.signOut}>Log Out</a>
       </li>
       <li>
         <NavLink to="/" className="btn btn-floating pink lighten-1">
@@ -19,4 +22,10 @@ const SignedInLinks = () => {
   );
 };
 
-export default SignedInLinks;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks)
