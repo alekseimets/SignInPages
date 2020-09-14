a6b9f088d5b468b0930dbfdb3ccbebc99669e967
alekseimets <alekseimets@gmail.com>
2020-09-14 14:48:20 +0300

    Update Index.js

--- Version_02/my-app/src/index.js
+++ Version_02/my-app/src/index.js
@@ -1,44 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './store/reducers/rootReducer'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import firebase from './config/fbConfig'
-import { createFirestoreInstance, reduxFirestore, getFirestore } from 'redux-firestore'
-import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import firebase from "./config/fbConfig";
+import {
+  createFirestoreInstance,
+  reduxFirestore,
+  getFirestore,
+} from "redux-firestore";
+import {
+  ReactReduxFirebaseProvider,
+  getFirebase,
+} from "react-redux-firebase";
+import { useSelector } from "react-redux";
+import { isLoaded } from "react-redux-firebase";
 
-import 'firebase/firestore';
+import "firebase/firestore";
 
-const rrfConfig = { 
-    userProfile: 'projects',
-    useFirestoreForProfile: true
-}
+const rrfConfig = {
+  userProfile: "projects",
+  useFirestoreForProfile: true,
+};
 
-const store = createStore(rootReducer, 
-    compose(
-        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-        reduxFirestore(firebase)
-    )
-    );
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebase)
+  )
+);
+
+function AuthIsLoaded({ children }) {
+    const auth = useSelector(state => state.firebase.auth)
+    if (!isLoaded(auth)) return <div>splash screen...</div>;
+    return children
+  }
 
 const rffProps = {
-    firebase,
-    useFirestoreForProfile: true,
-    config: rrfConfig,
-    dispatch: store.dispatch,
-    createFirestoreInstance
-}
+  firebase,
+  useFirestoreForProfile: true,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+};
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ReactReduxFirebaseProvider {...rffProps}>
-            <App />  
-        </ReactReduxFirebaseProvider>
-    </Provider>, 
-document.getElementById('root'));
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rffProps}>
+      <AuthIsLoaded>
+        <App />{" "}
+      </AuthIsLoaded>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
