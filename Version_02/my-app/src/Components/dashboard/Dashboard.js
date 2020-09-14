e14f450ddc4db54e9febf5cde3cb683b7e5206e0
alekseimets <alekseimets@gmail.com>
2020-09-14 15:05:55 +0300

    Update ProjectDetails, CreateProject and Dashboard about route guarting loginout

--- Version_02/my-app/src/Components/dashboard/Dashboard.js
+++ Version_02/my-app/src/Components/dashboard/Dashboard.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import Notifications from "./Notifications";
 import ProjectList from "../projects/ProjectList";
-import {connect} from 'react-redux';
-import {firestoreConnect} from 'react-redux-firebase'
-import {compose} from 'redux'
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import { Redirect } from "react-router-dom";
 
 class Dashboard extends Component {
   render() {
     //console.log(this.props);
-    const {projects} = this.props;
+    const { projects, auth } = this.props;
+    if (!auth.uid) return <Redirect to="/signin" />;
     return (
       <div className="dashboard container">
         <div className="row">
@@ -27,13 +29,12 @@ class Dashboard extends Component {
 const mapStateToProps = (state) => {
   console.log(state);
   return {
-    projects: state.firestore.ordered.projects
-  }
-}
+    projects: state.firestore.ordered.projects,
+    auth: state.firebase.auth,
+  };
+};
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect([
-    {collection: 'projects'}
-  ])
+  firestoreConnect([{ collection: "projects" }])
 )(Dashboard);
