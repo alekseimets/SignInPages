50784f3c5c92fd3438d0bb9cd8410ec920cc5c6b
alekseimets <alekseimets@gmail.com>
2020-09-15 14:23:19 +0300

    Update CreateProject

--- Version_02/my-app/src/Components/projects/CreateProject.js
+++ Version_02/my-app/src/Components/projects/CreateProject.js
@@ -19,36 +19,34 @@ class CreateProject extends Component {
     e.preventDefault();
     //console.log(this.state);
     this.props.createProject(this.state);
-    this.props.history.push("/");
+    this.props.history.push('/');
   };
 
   render() {
-    const { project, auth } = this.props;
+    const { auth } = this.props;
     if (!auth.uid) return <Redirect to="/signin" />;
-    if (project) {
-      return (
-        <div className="container">
-          <form onSubmit={this.handleSubmit} className="white">
-            <h5 className="grey-text text-darken-3">Ceate New Project</h5>
-            <div className="input-field">
-              <label htmlFor="title">Title</label>
-              <input type="text" id="title" onChange={this.handleChange} />
-            </div>
-            <div className="input-field">
-              <label htmlFor="content">Project Content</label>
-              <textarea
-                id="content"
-                className="materialize-textarea"
-                onChange={this.handleChange}
-              ></textarea>
-            </div>
-            <div className="input-field">
-              <button className="btn pink lighten-1 z-depth-0">Create</button>
-            </div>
-          </form>
-        </div>
-      );
-    }
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Ceate New Project</h5>
+          <div className="input-field">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="content">Project Content</label>
+            <textarea
+              id="content"
+              className="materialize-textarea"
+              onChange={this.handleChange}
+            ></textarea>
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Create</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
