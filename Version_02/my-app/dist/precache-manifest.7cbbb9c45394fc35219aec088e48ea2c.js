238f870d424e35c2af3084e1e0712c6c53742c6a
alekseimets <alekseimets@gmail.com>
2020-09-17 00:42:35 +0300

    Run Build and move to dist

---
+++ Version_02/my-app/dist/precache-manifest.7cbbb9c45394fc35219aec088e48ea2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25acfa8549630fc1fc23652ef2afdff1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "305fc51d0d21a9ce8448",
+    "url": "/static/css/main.ba03d887.chunk.css"
+  },
+  {
+    "revision": "a9c555b0b2e2b78d4dee",
+    "url": "/static/js/2.37549532.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.37549532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "305fc51d0d21a9ce8448",
+    "url": "/static/js/main.a562aa7d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
